3eac3b68328d5a7ef5b34e853ec135fee9b69267
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2024-01-03 23:48:40 +0900

    feat: initial commit

---
+++ src/main/java/com/neupinion/neupinion/NeupinionApplication.java
@@ -0,0 +1,13 @@
+package com.neupinion.neupinion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeupinionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeupinionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neupinion/neupinion/NeupinionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neupinion.neupinion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeupinionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
