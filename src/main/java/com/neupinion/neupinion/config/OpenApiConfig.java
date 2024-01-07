fa1bd2ef733289a6d0a16fb9c56465d2cac8e125
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2024-01-07 15:05:38 +0900

    feat: Swagger 설정

---
+++ src/main/java/com/neupinion/neupinion/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.neupinion.neupinion.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "neupinion API 명세서",
+        version = "v1"
+    )
+)
+public class OpenApiConfig {
+
+}
