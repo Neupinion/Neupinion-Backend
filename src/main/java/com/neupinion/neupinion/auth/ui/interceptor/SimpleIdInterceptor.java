217e14f902a930eea60d8c9029eb1021411e2e3e
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2024-06-04 14:57:42 +0900

    hotfix: 개발 서버 토큰 없는 경우 예외 처리

--- src/main/java/com/neupinion/neupinion/auth/ui/interceptor/SimpleIdInterceptor.java
+++ src/main/java/com/neupinion/neupinion/auth/ui/interceptor/SimpleIdInterceptor.java
@@ -4,6 +4,7 @@ import com.neupinion.neupinion.auth.ui.AuthContext;
 import com.neupinion.neupinion.member.application.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -20,9 +21,12 @@ public class SimpleIdInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
                              final Object handler) {
-        final String header = TokenHeaderExtractor.extractToken(request)
-                                             .orElse("1");
-        final long memberId = Long.parseLong(header);
+        final Optional<String> optionalToken = TokenHeaderExtractor.extractToken(request);
+        if (optionalToken.isEmpty()) {
+            authContext.setAuthenticatedMember(1L);
+            return true;
+        }
+        final long memberId = Long.parseLong(optionalToken.get());
         memberService.validateByIdThrowIfNotExist(memberId);
         authContext.setAuthenticatedMember(memberId);
 
