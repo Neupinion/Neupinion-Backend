7eb7ae2c61f9a556bd94f5af96cc3317bcb01311
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2024-06-04 15:27:58 +0900

    hotfix: 개발 서버 인터셉터 경로 추가

--- src/main/java/com/neupinion/neupinion/auth/config/DevAuthConfig.java
+++ src/main/java/com/neupinion/neupinion/auth/config/DevAuthConfig.java
@@ -1,6 +1,8 @@
 package com.neupinion.neupinion.auth.config;
 
 import com.neupinion.neupinion.auth.ui.argumentresolver.AuthArgumentResolver;
+import com.neupinion.neupinion.auth.ui.interceptor.PathMatcherInterceptor;
+import com.neupinion.neupinion.auth.ui.interceptor.PathMethod;
 import com.neupinion.neupinion.auth.ui.interceptor.SimpleIdInterceptor;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +22,16 @@ public class DevAuthConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(simpleIdInterceptor);
+        registry.addInterceptor(simpleIdInterceptor());
+    }
+
+    public PathMatcherInterceptor simpleIdInterceptor() {
+        return new PathMatcherInterceptor(simpleIdInterceptor)
+            .includePathPattern("/**", PathMethod.GET)
+            .includePathPattern("/**", PathMethod.PUT)
+            .includePathPattern("/**", PathMethod.POST)
+            .includePathPattern("/**", PathMethod.PATCH)
+            .includePathPattern("/**", PathMethod.DELETE);
     }
 
     @Override
