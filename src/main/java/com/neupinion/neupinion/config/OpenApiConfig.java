2c8c5e28a4d40fa1a6f54a707f7bf82aa423708a
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2024-06-07 20:15:01 +0900

    Feat/#65 토큰 재발급 API 구현 (#66)
    * feat: 리프레시 토큰 재발급 API 추가
    
    * config: 스웨거에서 API 전송 시 인증을 사용하도록 설정

--- src/main/java/com/neupinion/neupinion/config/OpenApiConfig.java
+++ src/main/java/com/neupinion/neupinion/config/OpenApiConfig.java
@@ -2,7 +2,10 @@ package com.neupinion.neupinion.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +22,12 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .addServersItem(new Server().url("https://dev.neupinion.com"));
+            .addServersItem(new Server().url("https://dev.neupinion.com"))
+            .components(new Components()
+                            .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
