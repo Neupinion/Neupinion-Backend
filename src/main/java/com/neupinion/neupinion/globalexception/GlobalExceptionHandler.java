5534ae1a833f86bbfa3a465dced05dfb0dcf07ec
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2024-06-07 16:50:39 +0900

    feat: 토큰 예외 처리 상태코드 추가

--- src/main/java/com/neupinion/neupinion/globalexception/GlobalExceptionHandler.java
+++ src/main/java/com/neupinion/neupinion/globalexception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.neupinion.neupinion.globalexception;
 
+import com.neupinion.neupinion.auth.exception.OAuthException;
+import com.neupinion.neupinion.auth.exception.TokenException;
 import com.neupinion.neupinion.issue.exception.CategoryException;
 import com.neupinion.neupinion.issue.exception.FollowUpIssueException;
 import com.neupinion.neupinion.issue.exception.IssueException;
@@ -7,6 +9,7 @@ import com.neupinion.neupinion.issue.exception.ParagraphException;
 import com.neupinion.neupinion.issue.exception.ReprocessedIssueException;
 import com.neupinion.neupinion.opinion.exception.OpinionException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,12 +20,19 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({
         IssueException.class, CategoryException.class, FollowUpIssueException.class, ReprocessedIssueException.class,
-        OpinionException.class, ParagraphException.class
+        OpinionException.class, ParagraphException.class, OAuthException.class
     })
     public ResponseEntity<ErrorResponse> handleGlobalBadRequestException(final CustomException e) {
         log.error(e.getErrorInfoLog());
 
         return ResponseEntity.badRequest().body(ErrorResponse.from(e));
+    }
+
+    @ExceptionHandler({TokenException.class})
+    public ResponseEntity<ErrorResponse> handleGlobalUnauthorizedException(final CustomException e) {
+        log.error(e.getErrorInfoLog());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.from(e));
     }
 
     @ExceptionHandler(CustomException.class)
