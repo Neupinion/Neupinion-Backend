b4eabe05b81bf9ba4c735d24fbfa78e40b96e3a6
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2024-06-11 17:14:35 +0900

    fix: API 요청마다 키워드를 계속해서 생성하도록 변경

--- src/main/java/com/neupinion/neupinion/article/application/KeywordService.java
+++ src/main/java/com/neupinion/neupinion/article/application/KeywordService.java
@@ -8,7 +8,6 @@ import com.neupinion.neupinion.issue.domain.ReprocessedIssueParagraph;
 import com.neupinion.neupinion.issue.domain.repository.IssueStandRepository;
 import com.neupinion.neupinion.issue.domain.repository.ReprocessedIssueParagraphRepository;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -26,34 +25,18 @@ public class KeywordService {
 
     @Transactional
     public KeywordResponse getKeywords(final Long issueId) {
-        final List<IssueKeyword> keywords = issueKeywordRepository.findByIssueId(issueId);
         final List<IssueStand> stands = issueStandRepository.findByIssueIdOrderById(issueId);
-        if (keywords.isEmpty()) {
-            return extractKeyword(issueId, stands);
-        }
-        final IssueStand firstStand = stands.get(0);
-        final IssueStand secondStand = stands.get(1);
-        return new KeywordResponse(
-            firstStand.getStand(),
-            keywords.stream()
-                    .filter(keyword -> Objects.equals(keyword.getIssueStandId(), firstStand.getId()))
-                    .map(IssueKeyword::getKeyword)
-                    .toList(),
-            secondStand.getStand(),
-            keywords.stream()
-                    .filter(keyword -> Objects.equals(keyword.getIssueStandId(), secondStand.getId()))
-                    .map(IssueKeyword::getKeyword)
-                    .toList());
+        return extractKeyword(issueId, stands);
     }
 
     private KeywordResponse extractKeyword(final Long issueId, final List<IssueStand> stands) {
         final String articleBody = reprocessedIssueParagraphRepository.findByReprocessedIssueIdOrderById(issueId)
-                                                                      .stream()
-                                                                      .map(ReprocessedIssueParagraph::getContent)
-                                                                      .collect(Collectors.joining("\n"));
+            .stream()
+            .map(ReprocessedIssueParagraph::getContent)
+            .collect(Collectors.joining("\n"));
 
         final KeywordResponse response = chatGptService.getKeywords(articleBody, stands)
-                                                       .block();
+            .block();
         response.getFirstKeywords().forEach(keyword -> issueKeywordRepository.save(
             IssueKeyword.forSave(keyword, stands.get(0).getId(), issueId)
         ));
